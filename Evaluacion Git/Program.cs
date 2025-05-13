4ef39ab3657b4c9c1a7988b2c00ca5296b9b26be
PULSE <PULSE@MSI>
2025-05-13 14:51:48 -0600

    Agregar archivos de proyecto.

---
+++ Evaluacion Git/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Evaluacion_Git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
